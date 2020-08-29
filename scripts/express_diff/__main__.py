953aede387a170b8734f9020d98cb939366873b6
Thomas Krijnen <t.krijnen@gmail.com>
2020-08-29 12:09:52 +0200

    Add where, unique, derive rules and attribute aggregate qualifiers to EXP comparison

--- scripts/express_diff/__main__.py
+++ scripts/express_diff/__main__.py
@@ -1,10 +1,13 @@
+import os
 import sys
 import operator
 
 import tabulate
 
-from . import express_parser
-from . import nodes
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
+import express_parser
+import nodes
 
 fn1, fn2, output = sys.argv[1:]
 
@@ -13,25 +16,23 @@ print("Running difference", *sys.argv[1:])
 def eq(a, b):
     if (type(a) != type(b)):
         return False
-    elif isinstance(a, (nodes.StringType, nodes.AggregationType, nodes.BinaryType, str, nodes.ExplicitAttribute, nodes.InverseAttribute)):
+    elif isinstance(a, (nodes.StringType, nodes.AggregationType, nodes.BinaryType, str, nodes.ExplicitAttribute, nodes.InverseAttribute, nodes.SimpleType)):
         return str(a) == str(b)
     elif isinstance(a, (nodes.SelectType, nodes.EnumerationType)):
-        return set(a.values) == set(b.values)
-    elif isinstance(a, nodes.EntityDeclaration):
-        # @todo
-        import pdb; pdb.set_trace()
-        
-        return str(a) == str(b)
+        return set(map(str, a.values)) == set(map(str, b.values))
     else:
+        # @todo
         import pdb; pdb.set_trace()
         
 def format(a):
     if isinstance(a, (nodes.SelectType, nodes.EnumerationType)):
-        return ", ".join(sorted(a.values))
+        return ", ".join(sorted(map(str, a.values)))
     else:
         return str(a)
 
-def compare(fn1, fn2, s1, s2):
+def compare(fn1, fn2, m1, m2):
+    s1, s2 = m1.schema, m2.schema
+    
     for nm in set(s1.keys) - set(s2.keys):
         yield (nm, "", "not in '%s'" % fn2)
         
@@ -43,6 +44,16 @@ def compare(fn1, fn2, s1, s2):
         for nm in sorted(d1.keys() & d2.keys()):
             if not eq(d1[nm], d2[nm]):
                 yield (nm, d1[nm], d2[nm])
+            if x == 'simpletypes':
+                d1_wns = [a[0] for a in s1.types[nm].where]
+                d2_wns = [a[0] for a in s2.types[nm].where]
+                if d1_wns != d2_wns:
+                    yield (nm + " where rules", d1_wns, d2_wns)
+                for wnm in (set(d1_wns) & set(d2_wns)):
+                    w1 = dict(s1.types[nm].where)[wnm]
+                    w2 = dict(s2.types[nm].where)[wnm]
+                    if w1 != w2:
+                        yield (nm + "." + wnm, w1, w2)
             
     for e in sorted(s1.entities.keys() & s2.entities.keys()):
         e1, e2 = s1.entities[e], s2.entities[e]
@@ -64,13 +75,26 @@ def compare(fn1, fn2, s1, s2):
         e1_anames = [attr.name for attr in (e1.inverse or [])]
         e2_anames = [attr.name for attr in (e2.inverse or [])]
         if e1_anames != e2_anames:
-            yield (e + "." + nm + " inverses", e1_anames, e2_anames)
+            yield (e + " inverses", e1_anames, e2_anames)
         e1_anames = {attr.name:attr for attr in (e1.inverse or [])}
         e2_anames = {attr.name:attr for attr in (e2.inverse or [])}
         for nm in e1_anames.keys() & e2_anames.keys():
             if not eq(e1_anames[nm], e2_anames[nm]):
                 yield (e + "." + nm, e1_anames[nm], e2_anames[nm])
-                
+        
+        for constraint in ("where", "unique", "derive"):            
+            cv1 = getattr(s1.entities[e], constraint)
+            cv2 = getattr(s2.entities[e], constraint)
+            d1_wns = [a[0] for a in cv1]
+            d2_wns = [a[0] for a in cv2]
+            if d1_wns != d2_wns:
+                yield (e + " " + constraint +  " rules", d1_wns, d2_wns)
+            for wnm in (set(d1_wns) & set(d2_wns)):
+                w1 = dict(cv1)[wnm]
+                w2 = dict(cv2)[wnm]
+                if w1 != w2:
+                    yield (e + "." + wnm, w1, w2)
+        
 with open(output, "w") as f:
     f.write(tabulate.tabulate(list(
         [format(x) for x in tup] for tup in compare(fn1, fn2, *map(express_parser.parse, (fn1, fn2)))

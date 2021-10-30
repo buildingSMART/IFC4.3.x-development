765efa7307394443c81409a043d2c3a7e0627ebd
Thomas Krijnen <t.krijnen@gmail.com>
2021-10-30 15:54:30 +0200

    Parse concept rules. Sort templates based on rule parameters. Write data types to excel sheets. Correct parameter grouping.

--- scripts/concept_extractor.py
+++ scripts/concept_extractor.py
@@ -16,6 +16,18 @@ import xlsxwriter
 
 flatten=lambda l: sum(map(flatten,l),[]) if isinstance(l,(list, pyparsing.ParseResults)) else [l]
 
+class capturer:
+    """
+    Captures template rule identification    
+    """
+    def __init__(self, root=None):
+        self.mapping = {}
+        self.root = root
+        
+    def __call__(self, rule, parent):
+        if rule.bind:
+            self.mapping[rule.bind] = ((parent.attribute if parent else self.root), rule.attribute)
+
 try:
     fn = sys.argv[1]
 except:
@@ -24,23 +36,39 @@ except:
 def dump(rule, parents, file=sys.stdout): print(" " * len(parents), "- ", rule.attribute, sep='', file=file)
 
 roots = list(mvd.concept_root.parse(fn))
-
 grouping = defaultdict(list)
+bindings = {}
 
 for root in roots:
     for concept in root.concepts():
-        parameters = defaultdict(list)
-        for k, v in map(operator.attrgetter('a', 'c'), filter(lambda x: isinstance(x, expression_node), flatten(concept.rules()))):
-            parameters[k].append(v)
+        tmpl = concept.template()
+        uuid = tmpl.root.getAttribute("uuid")
+        if uuid in bindings:
+            ks = bindings[uuid]
+        else:
+            capture_binding = capturer(tmpl.entity)
+            tmpl.traverse(capture_binding)
+            ks = tuple(sorted(capture_binding.mapping.items()))
+            bindings[uuid] = ks
             
-        ks = tuple(sorted(parameters.keys()))
+        if concept.name == "Property Sets for Objects" and root.entity == "IfcDiscreteAccessory":
+            import pdb; pdb.set_trace()
+    
+        parameters = defaultdict(list)
+        param_keys = dict(ks).keys()
+        for rl in concept.rules():
+            if rl == "and": continue
+            prms = dict(map(operator.attrgetter('a', 'c'), filter(lambda x: isinstance(x, expression_node), flatten(rl))))
+            for kk in param_keys:
+                parameters[kk].append(prms.get(kk))
+        
         key = (concept.name,) + ks
         
-        for vals in itertools.zip_longest(*[parameters[k] for k in ks]):
+        for vals in itertools.zip_longest(*[parameters.get(k[0], []) for k in ks]):
             grouping[key].append((root.entity,) + tuple(vals))
             
         if len(parameters) == 0:
-            grouping[key].append((root.entity,))
+            grouping[key].append((root.entity,) + tuple(None for k in ks))
             
 def format(v):
     if isinstance(v, str) and v[0] == "'" and v[-1] == "'":
@@ -51,19 +79,24 @@ workbook = xlsxwriter.Workbook('concepts.xlsx')
 header_format = workbook.add_format({'bg_color': 'black', 'font_color': 'white'})
 postfixes = defaultdict(int)
 for k, vss in grouping.items():
-    sheet_name = k[0].replace(" ", "")[0:25]
-    if postfixes[k[0]]:
-        sheet_name += " %02d" % postfixes[k[0]]
-    postfixes[k[0]] += 1
+    sheet_name_orig = sheet_name = k[0].replace(" ", "")[0:25]
+    
+    if postfixes[sheet_name_orig.lower()]:
+        sheet_name += " %02d" % postfixes[sheet_name_orig.lower()]
+    postfixes[sheet_name_orig.lower()] += 1
+    
+    columns_in_use = [i for i, xs in enumerate(zip(*vss)) if any(x is not None for x in xs)]
     
     worksheet = workbook.add_worksheet(sheet_name)
     worksheet.write(0, 0, "ApplicableEntity", header_format)
-    for j, h in enumerate(k[1:]):
-        worksheet.write(0, j + 1, h, header_format)
+    worksheet.write(1, 0, " ", header_format)
+    for j, h in enumerate([k[col] for col in columns_in_use][1:]):
+        worksheet.write(0, j + 1, h[0], header_format)
+        worksheet.write(1, j + 1, ".".join(h[1]), header_format)
         
     for i, vs in enumerate(vss):
-        for j, v in enumerate(vs):
-            worksheet.write(i + 1, j, format(v))
+        for j, v in enumerate([vs[col] for col in columns_in_use]):
+            worksheet.write(i + 2, j, format(v))
             
 for h in sorted(postfixes.keys()):
     print(h)

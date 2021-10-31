0c2e4d177a3515f9144d43f11fa1f637516aa0ca
Thomas Krijnen <t.krijnen@gmail.com>
2021-10-31 17:00:25 +0100

    Work on importing more concepts

--- scripts/import_concepts_to_xmi.py
+++ scripts/import_concepts_to_xmi.py
@@ -1,29 +1,153 @@
 import sys
+import operator
 
 import append_xmi
 import concept_extractor
 
+from append_xmi import XMI
+
+def norm(v):
+    return v.lower().replace(" ", "").replace("_", "")
+
 if __name__ == "__main__":
     fn = sys.argv[1]
     
-    c = append_xmi.context("..\schemas\IFC.xml")
-    ifc_package = c.package_by_name("IFC4x3_RC4")
-    views_package = c.insert(ifc_package, append_xmi.uml_package("Views"))
-    gu_package = c.insert(views_package, append_xmi.uml_package("GeneralUsage"))
+    ctx = append_xmi.context("..\schemas\IFC.xml")
+    ifc_package = ctx.package_by_name("IFC4x3_RC4")
+    views_package = ctx.insert(ifc_package, append_xmi.uml_package("Views"))
+    gu_package = ctx.insert(views_package, append_xmi.uml_package("GeneralUsage"))
     
     x = concept_extractor.extractor(fn)
     
+    realizations = set()
+    def v(nd, _):
+        if nd.attributes.get(XMI.type) == "uml:Realization":
+            realizations.add(frozenset((nd.attributes["supplier"], nd.attributes["client"])))
+    ctx._recurse(v)
+    
     # Axis Geometry
-    axis_geom_package = c.insert(gu_package, append_xmi.uml_package("AxisGeometry"))    
+    axis_geom_package = ctx.insert(gu_package, append_xmi.uml_package("AxisGeometry"))
     axis_geom = append_xmi.uml_class("AxisGeometry")
-    c.insert(axis_geom_package, axis_geom)
+    ctx.insert(axis_geom_package, axis_geom)
     
     axis_concepts = x.concept_starting_with("Axis ")
     axis_entities = set(v[0] for v in axis_concepts)
     
     for entity in axis_entities:
-        ids = [axis_geom.id, c.to_id[("uml:Class", entity)]]
+        ids = [axis_geom.id, ctx.to_id("uml:Class", entity)]
         assoc = append_xmi.uml_assoc_class(f"{entity}AxisGeometryUsage", ids)
-        c.insert(axis_geom_package, assoc)
+        ctx.insert(axis_geom_package, assoc)
         
-    c.write("..\schemas\IFC_with_concepts.xml")
+    # Port Nesting
+    concept_name = "Port Nesting"
+    concept_name_short = concept_name.replace(" ", "")
+    concept_package = ctx.insert(gu_package, append_xmi.uml_package(concept_name_short))
+    
+    key = [k for k in x.grouping.keys() if k[0] == concept_name][0]
+    parameters = key[1:]
+    values = x.grouping[key]
+    
+    parameter_id_mapping = [{} for _ in parameters]
+    
+    for column_id, ((rule_id, (entity_name, attribute_name)), pmap) in enumerate(zip(parameters, parameter_id_mapping), start=1):
+
+        nd = ctx.to_node[entity_name]
+        while True:
+            attrs = [ch for ch in nd.children if ch.attributes[XMI.type] == "uml:Property" and ch.attributes['name'] == attribute_name]
+            if attrs:
+                attr_nodes = [attrs[0]]
+                break
+            else:
+                gens = [ch for ch in nd.children if ch.tag == "generalization"]
+                if gens:
+                    nd = ctx.to_node[gens[0].attributes['general']]
+                else:
+                    attr_nodes = None
+                    break
+                    
+        if attr_nodes is None:
+        
+            def subclasses(x):
+                yield x
+                for s in ctx.subclasses[x]:
+                    yield from subclasses(s)
+                    
+            scs = list(subclasses(entity_name))
+            
+            attr_nodes = []
+            
+            for sc in scs:
+                attrs = [ch for ch in ctx.to_node[sc].children if ch.attributes[XMI.type] == "uml:Property" and ch.attributes['name'] == attribute_name]
+                if attrs:
+                    attr_nodes.append(attrs[0])            
+        
+        for attr in attr_nodes:
+            
+            attr_type_id = [ch for ch in attr.children if ch.tag == "type"][0].attributes[XMI.idref]
+            attr_type = ctx.to_node[attr_type_id]
+            attr_type_name = attr_type.attributes['name']
+            
+            if attr_type.attributes[XMI.type] == "uml:Enumeration":
+            
+                enum_values = [ch.attributes['name'] for ch in attr_type.children if ch.tag == "ownedLiteral"]
+                for ev in enum_values:
+                    full_name = f"{attr_type_name}.{ev}"
+                    if full_name in ctx.to_node:
+                        vid = pmap[norm(ev)] = ctx.to_node[full_name].attributes[XMI.id]
+                        eid = attr_type.attributes[XMI.id]
+                        ids = (vid, eid)
+                        if frozenset(ids) not in realizations:
+                            realizations.add(frozenset(ids))
+                            ctx.insert(attr_type.parent, append_xmi.uml_realization(*ids))
+                    else:
+                        assert attr_type.parent.attributes[XMI.type] == "uml:Package"
+                        enum_value_class = append_xmi.uml_class(full_name)
+                        ctx.insert(attr_type.parent, enum_value_class)
+                        ids = (enum_value_class.id, attr_type.attributes[XMI.id])
+                        realizations.add(frozenset(ids))
+                        ctx.insert(attr_type.parent, append_xmi.uml_realization(*ids))
+                        pmap[norm(ev)] = enum_value_class.id
+            
+            else:
+            
+                assert len(attrs) == 1
+            
+                column = sorted(set(v[column_id] for v in values))
+                new_enum = append_xmi.uml_enumeration(f"{concept_name_short}{rule_id}Values", column)
+                ctx.insert(concept_package, new_enum)
+                for col in column:
+                    full_name = f"{concept_name_short}{rule_id}Values.{col}"
+                    enum_value_class = append_xmi.uml_class(full_name)
+                    ctx.insert(concept_package, enum_value_class)
+                    ctx.insert(concept_package, append_xmi.uml_realization(enum_value_class.id, new_enum.id))
+                    pmap[norm(col)] = enum_value_class.id
+
+    def lookup_and_warn(i, pmap, p):
+        v = pmap.get(norm(p))
+        if v is None:
+            print(f"Not valid: '{p}' for type {parameters[i][1][0]}.{parameters[i][1][1]} on parameter '{parameters[i][0]}'")
+        return v
+                
+    for row in values:
+        entity = row[0]
+        params = row[1:]
+        ids = list(filter(None, [lookup_and_warn(i, pmap, p) for i, (pmap, p) in enumerate(zip(parameter_id_mapping, params)) if p]))
+        ids = [ctx.to_id("uml:Class", entity)] + ids
+        assoc = append_xmi.uml_assoc_class(f"{entity}{concept_name_short}Usage", ids, type="uml:Association")
+        ctx.insert(concept_package, assoc)
+        
+    # Object Typing
+    concept_name = "Object Typing"
+    concept_name_short = concept_name.replace(" ", "")
+    concept_package = ctx.insert(gu_package, append_xmi.uml_package(concept_name_short))
+    
+    key = [k for k in x.grouping.keys() if k[0] == concept_name][0]
+    parameters = key[1:]
+    values = x.grouping[key]
+    column_id = list(map(operator.itemgetter(0), parameters)).index("RelatingType") + 1
+    for ent_ty in [(row[0], row[column_id]) for row in values]:
+        ids = [ctx.to_id("uml:Class", x) for x in ent_ty]
+        assoc = append_xmi.uml_assoc_class(f"{ent_ty[0]}{concept_name_short}Usage", ids)
+        ctx.insert(concept_package, assoc)
+        
+    ctx.write("..\schemas\IFC_with_concepts.xml")

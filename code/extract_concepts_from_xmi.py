1f5ef92199c6bbae5102bb675762f2d737969284
Thomas Krijnen <t.krijnen@gmail.com>
2021-12-28 14:54:40 +0100

    Reimplement template parametrization extraction using automatic rule binding

--- code/extract_concepts_from_xmi.py
+++ code/extract_concepts_from_xmi.py
@@ -15,14 +15,6 @@ try:
 except:
     fn = os.path.join(os.path.dirname(__file__), '..', 'schemas', 'IFC.xml')
 
-xmi_doc = xmi_document(fn)
-# ctx = extractor("../mvdXML/IFC4_ADD2.mvdxml")
-
-definitions = list(xmi_doc)
-definitions_by_name = {x.name: x for x in definitions}
-entities = [e for e in definitions if e.type == "ENTITY"]
-name_to_id = {c.name: c.id for c in xmi_doc.xmi.by_tag_and_type["packagedElement"]["uml:Class"]}
-
 def get_predefined_types_enum(e):
     # can be an xmi_item in case of selects, otherwise express definition
     vs = e.values if hasattr(e, 'values') else e.definition.values
@@ -41,18 +33,45 @@ def get_predefined_types_for_entity(e):
         else:
             yield from get_predefined_types_enum(type_def)
 
+xmi_doc = xmi_document(fn)
+
+definitions = list(xmi_doc)
+definitions_by_name = {x.name: x for x in definitions}
+
+def yield_supertypes_items(a):
+    yield a
+    try:
+        yield from yield_supertypes_items(definitions_by_name[a.definition.supertype])
+    except: pass
+
+non_type_definitions = [x for x in definitions if definitions_by_name["IfcObject"] in yield_supertypes_items(x)]
+
+entities = [e for e in definitions if e.type == "ENTITY"]
+name_to_node = {c.name: c for c in xmi_doc.xmi.by_tag_and_type["packagedElement"]["uml:Class"]}
+name_to_id = {c.name: c.id for c in xmi_doc.xmi.by_tag_and_type["packagedElement"]["uml:Class"]}
+
+# @nb this includes qsets in the xmi_document iteration,
+#     where qsets are a separate stereotype within pset
+psets_and_qsets = set(n.node.idref for n in definitions if n.type == 'PSET')
+
+def get_predefined_type_listing(items):
+    return dict((e.node.idref, [name_to_node[x] \
+    for x in get_predefined_types_for_entity(e)]) for e in items if list(get_predefined_types_for_entity(e)))
+
+entity_to_predtypes = get_predefined_type_listing(entities)
+all_predtypes = sum(entity_to_predtypes.values(), [])
+all_predtype_ids = set(x.id for x in all_predtypes)
+
+predtype_to_entity = dict((v.id, k) for k, vs in entity_to_predtypes.items() for v in vs)
+
+# proritize predtype->entity mappings for the subtypes of ifcobject as opposed to ifctypeobject
+entity_to_predtypes_non_type = get_predefined_type_listing(non_type_definitions)
+predtype_to_entity_non_type = dict((v.id, k) for k, vs in entity_to_predtypes_non_type.items() for v in vs)
+predtype_to_entity.update(predtype_to_entity_non_type)
+
 result = defaultdict(list)
 
 all_templates = glob.glob(os.path.join(os.path.dirname(fn), "../docs/templates/**/*.md"), recursive=True)
-  
-    
-def flatten(iterable):
-    if isinstance(iterable, (list, tuple)):
-        for x in iterable:
-            yield from flatten(x)
-    else:
-        yield iterable
-
 
 def get_all_attributes(nm):
     d = dict(definitions_by_name[nm].definition.attributes)
@@ -67,12 +86,35 @@ def get_all_attributes(nm):
     return d
 
 
+class predefined_type_attribute: pass
+
+class property_set_class: pass
+
 def parse_bindings(concept):
     tmpl = [t for t in all_templates if os.path.abspath(t).split(os.sep)[-2].lower().replace(" ", "") == concept.lower()][0]
     concept_block = re.findall(r"concept\s*\{.+?\}", open(tmpl, encoding='utf-8').read(), flags=re.S)[0]
     for a,b,c in re.findall(r'(\w+):(\w+)\[binding="(.+?)"\]', concept_block):
-        t = get_all_attributes(a).get(b).replace("OPTIONAL ", "")
-        yield c, (a,b), xmi_doc.xmi.by_id[definitions_by_name[t].id]
+        t = get_all_attributes(a).get(b, '').replace("OPTIONAL ", "").replace("UNIQUE ", "")
+        
+        if (a,b) == ('IfcPropertySet', 'Name'):
+            # In UML we have property sets as defined classes, which in
+            # IFC are fully late-bound type definitions. Selection of the
+            # appropriate psets happens through Name, which is IfcLabel
+            # which will match the pset class name in UML.
+            type_node = property_set_class()
+        elif not t and b == "PredefinedType":
+            # PredefinedType is not really an attribute with a specific
+            # type in the sense that all entities define their own version
+            # of the attribute and there isn't a general basis for all
+            # enumerations.
+            type_node = predefined_type_attribute()
+        else:
+            if " OF " in t:
+                # strip off aggregate information
+                t = t.split(" OF ")[1]
+            type_node = xmi_doc.xmi.by_id[definitions_by_name[t].id]
+            
+        yield c, (a,b), type_node
     
 
 def yield_supertypes(a):
@@ -85,48 +127,18 @@ def yield_supertypes(a):
 def make_get_binding(bindings):
     def inner(el):
         for nm, attr, nd in bindings:
-            if nd in set(yield_supertypes(el)):
+            if isinstance(nd, predefined_type_attribute):
+                if el.id in all_predtype_ids:
+                    return nm
+            elif isinstance(nd, property_set_class):
+                if el.id in psets_and_qsets:
+                    return nm
+            elif nd in set(yield_supertypes(el)):
                 return nm
     return inner
-
-for xmi_concept, v in xmi_doc.concepts.items():
-    for e in entities:
-        
-        # mvd_concept = [k for k in ctx.grouping.keys() if k[0].replace(" ", "") == xmi_concept][0]
-        
-        if xmi_concept == "PropertySetsforObjects":
-            # binding_names = [x[0] for x in mvd_concept[1:]] 
-            binding_names = "ApplicableEntity", "PredefinedType", "PsetName"
-            if e.id in v:
-                for binding in v[e.id]:
-                    if not isinstance(binding, tuple):
-                        # @todo investigate
-                        binding = (binding,)
-                    others = list(map(xmi_doc.xmi.by_id.__getitem__, binding))
-                    result[xmi_concept].append(dict(zip(binding_names, [e.name, "", others[0].name])))
-                        
-            for pt in get_predefined_types_for_entity(e):
-                if name_to_id.get(pt) and name_to_id.get(pt) in v:
-                    for binding in v[name_to_id.get(pt)]:
-                        if not isinstance(binding, tuple):
-                            # @todo investigate
-                            binding = (binding,)
-                        others = list(map(xmi_doc.xmi.by_id.__getitem__, binding))
-                        result[xmi_concept].append(dict(zip(binding_names, [e.name, pt.split(".")[1], others[0].name])))
-
-    if xmi_concept == "SpatialContainment":
-        bindings = list(parse_bindings(xmi_concept))
-        get_binding = make_get_binding(bindings)
-        for k, vs in v.items():
-            for vv in vs:
-                d = {
-                    'ApplicableEntity': xmi_doc.xmi.by_id[k].name,
-                    get_binding(xmi_doc.xmi.by_id[vv]): xmi_doc.xmi.by_id[vv].name
-                }
-                result[xmi_concept].append(d)
                     
 for xmi_concept, pairs in xmi_doc.concept_associations.items():
-    if xmi_concept == "ObjectTyping":
+    if xmi_concept in ("PropertySetsforObjects", "SpatialContainment", "ObjectTyping"):
         bindings = list(parse_bindings(xmi_concept))
         get_binding = make_get_binding(bindings)
         
@@ -134,6 +146,12 @@ for xmi_concept, pairs in xmi_doc.concept_associations.items():
             elems = tuple(map(xmi_doc.xmi.by_id.__getitem__, p))
             elem_binds = list(map(get_binding, elems))
             
+            predtype_bind = all_predtype_ids & set(e.id for e in elems)
+            if xmi_concept == "PropertySetsforObjects" and predtype_bind:
+                # when binding to a predefined type, we can infer the ApplicableEntity
+                elem_binds.append(None)
+                elems += (xmi_doc.xmi.by_id[predtype_to_entity[next(iter(predtype_bind))]],)
+                
             # a single element should not be bound, that is the ApplicableEntity
             assert len([x for x in elem_binds if x is None]) == 1
             elem_binds[elem_binds.index(None)] = "ApplicableEntity"

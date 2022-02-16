a11d4c05c725d4e9d29503fa35766d32e4a2355a
Dion Moult <dion@thinkmoult.com>
2022-02-16 12:27:02 +1100

    Add ability to quickly jump to sections, especialy useful for long pages

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -29,137 +29,171 @@ function refreshCollapsed(element) {
     }
 }
 
-let elements = document.querySelectorAll('h2, h3, h4, h5, h6');
-for (let i=0; i<elements.length; i++) {
-    let element = elements[i];
-    renderToggleIcon(element);
-    refreshCollapsed(element);
+function makeHeadersCollapsible() {
+    let elements = document.querySelectorAll('h2, h3, h4, h5, h6');
+    for (let i=0; i<elements.length; i++) {
+        let element = elements[i];
+        renderToggleIcon(element);
+        refreshCollapsed(element);
 
-    element.onclick = function(e) {
-        let target = e.target;
-        if (target.classList.contains('feather-link')) {
-            return;
+        element.onclick = function(e) {
+            let target = e.target;
+            if (target.classList.contains('feather-link')) {
+                return;
+            }
+            while (target.tagName.slice(0, 1) != "H") {
+                target = target.parentNode;
+            }
+            target.classList.toggle('collapsed');
+            refreshCollapsed(target);
         }
-        while (target.tagName.slice(0, 1) != "H") {
-            target = target.parentNode;
+    }
+}
+
+function generateSectionNavigation() {
+    let content = document.getElementById('main-content');
+    let nav = document.getElementById('section-navigation');
+    let ol = nav.getElementsByTagName('ol')[0];
+    Array.from(content.getElementsByTagName('h2')).forEach((h2) => {
+        let href = null;
+        Array.from(h2.getElementsByTagName('a')).forEach((anchor) => {
+            if (anchor.getAttribute('href')) {
+                href = anchor.getAttribute('href');
+            }
+        });
+        if (href) {
+            let name = h2.textContent.trim();
+            let number = name.split(' ', 1)[0];
+            name = name.substring(number.length);
+
+            li = document.createElement('li');
+            li.setAttribute('number', number);
+            a = document.createElement('a');
+            a.setAttribute('href', href);
+            a.textContent = name;
+            li.append(a);
+            ol.append(li);
         }
-        target.classList.toggle('collapsed');
-        refreshCollapsed(target);
+    });
+    if (ol.getElementsByTagName('li').length) {
+        nav.classList.remove('hidden');
     }
 }
 
 Array.from(document.querySelectorAll('a')).concat(Array.from(document.querySelectorAll('em'))).forEach((a) => {
-  let popup = null;
-  let timeout = null;
-  let onElem = false;
-  let onPopup = false;
-  
-  let clearPopup = () => {
-    if (onElem || onPopup) {
-      return false;
-    }
-    if (popup) {
-      document.body.removeChild(popup);
-    }
-    popup = null;  
-  };
-  
-  let soonClearPopup = () => {
-    setTimeout(clearPopup, 1000.);
-  };
-  
-  let showPopup = () => {
-    if (!onElem) {
-      return false;
-    }
-    fetch('/api/v0/resource/' + a.innerText).then(r => r.json()).then(data => {
-      if (popup) {
-        onElem = true;
-        return;
-      }
-      popup = document.createElement('div');
-      h1 = document.createElement('h1');
-      p = document.createElement('div');
-      table = document.createElement('table');
-      h1.appendChild(document.createTextNode(data.resource));
-      p.innerHTML = data.definition;
-      data.attributes.forEach(a => {
-        tr = document.createElement('tr');
-        let inv = false;
-        a.forEach((c,i) => {
-          if (i === 0 && c === '') {
-            inv = true;
-          } else if (i === 3) {
-            // don't add definition
-            return;
-          }
-          td = document.createElement('td');
-          tr.appendChild(td)
-          if (inv) {
-            ii = document.createElement('i');
-            ii.style.opacity = 0.7;
-            td.appendChild(ii);
-            td = ii;
-          }
-          td.appendChild(document.createTextNode(c));
+    let popup = null;
+    let timeout = null;
+    let onElem = false;
+    let onPopup = false;
+
+    let clearPopup = () => {
+        if (onElem || onPopup) {
+            return false;
+        }
+        if (popup) {
+            document.body.removeChild(popup);
+        }
+        popup = null;
+    };
+
+    let soonClearPopup = () => {
+        setTimeout(clearPopup, 1000.);
+    };
+
+    let showPopup = () => {
+        if (!onElem) {
+            return false;
+        }
+        fetch('/api/v0/resource/' + a.innerText).then(r => r.json()).then(data => {
+            if (popup) {
+                onElem = true;
+                return;
+            }
+            popup = document.createElement('div');
+            h1 = document.createElement('h1');
+            p = document.createElement('div');
+            table = document.createElement('table');
+            h1.appendChild(document.createTextNode(data.resource));
+            p.innerHTML = data.definition;
+            data.attributes.forEach(a => {
+                tr = document.createElement('tr');
+                let inv = false;
+                a.forEach((c,i) => {
+                    if (i === 0 && c === '') {
+                        inv = true;
+                    } else if (i === 3) {
+                        // don't add definition
+                        return;
+                    }
+                    td = document.createElement('td');
+                    tr.appendChild(td)
+                    if (inv) {
+                        ii = document.createElement('i');
+                        ii.style.opacity = 0.7;
+                        td.appendChild(ii);
+                        td = ii;
+                    }
+                    td.appendChild(document.createTextNode(c));
+                });
+                table.appendChild(tr)
+            });
+            popup.className = 'popup';
+            popup.appendChild(h1);
+            popup.appendChild(p);
+            popup.appendChild(table);
+            document.body.appendChild(popup);
+
+            popup.onmouseenter = () => {
+                onPopup = true;
+            }
+
+            popup.onmouseleave = () => {
+                onPopup = false;
+                soonClearPopup();
+            };
+
+            let rect = a.getBoundingClientRect();
+            popup.style.left = ((rect.left + rect.right) / 2. - 250 + window.scrollX) + 'px';
+            popup.style.top = (rect.bottom + 10. + window.scrollY) + 'px';
         });
-        table.appendChild(tr)
-      });
-      popup.className = 'popup';
-      popup.appendChild(h1);
-      popup.appendChild(p);
-      popup.appendChild(table);
-      document.body.appendChild(popup);
-      
-      popup.onmouseenter = () => {
-        onPopup = true;
-      }
-      
-      popup.onmouseleave = () => {
-        onPopup = false;
+    }
+
+    let soonShowPopup = () => {
+        timeout = setTimeout(showPopup, 1000.);
+    };
+
+    a.onmouseenter = () => {
+        onElem = true;
+        soonShowPopup();
+    };
+
+    a.onmouseleave = () => {
+        onElem = false;
         soonClearPopup();
-      };
-      
-      let rect = a.getBoundingClientRect();
-      popup.style.left = ((rect.left + rect.right) / 2. - 250 + window.scrollX) + 'px';
-      popup.style.top = (rect.bottom + 10. + window.scrollY) + 'px';    
-    });
-  }
+        clearTimeout(timeout);
+    };
 
-  let soonShowPopup = () => {
-    timeout = setTimeout(showPopup, 1000.);
-  };
-  
-  a.onmouseenter = () => {
-    onElem = true;
-    soonShowPopup();
-  };
-
-  a.onmouseleave = () => {
-    onElem = false;
-    soonClearPopup();
-    clearTimeout(timeout);
-  };
-  
 });
 
 fetch(`https://api.github.com/repos/${window.appconfig.repo}/commits?path=${window.appconfig.path}`).then(r => r.json()).then(j => {
-  let n = {};
-  j.forEach(c => {
-    n[c.author.avatar_url] = (n[c.author.avatar_url || 0]) + 1;
-  });
-  let es = Object.entries(n);
-  document.getElementById('contributors').appendChild(
-    document.createTextNode(es.length + ' contributor(s): ')
-  );
-  es.map(kv => [kv[1], kv[0]]).sort().forEach(kv => {
-    let img = document.createElement('img');
-    img.src = kv[1] + '&s=32';
-    img.classList.add('contributor-icon');
-    document.getElementById('contributors').appendChild(img);
-  });
-  document.getElementById('last-change').innerHTML += 'Last change: ' +
-  '<em>' + j[0].commit.message + '</em>' + ' by ' + j[0].commit.author.name + ' on ' + (new Date(j[0].commit.author.date)).toLocaleString();
+    let n = {};
+    j.forEach(c => {
+        n[c.author.avatar_url] = (n[c.author.avatar_url || 0]) + 1;
+    });
+    let es = Object.entries(n);
+    document.getElementById('contributors').appendChild(
+        document.createTextNode(es.length + ' contributor(s): ')
+    );
+    es.map(kv => [kv[1], kv[0]]).sort().forEach(kv => {
+        let img = document.createElement('img');
+        img.src = kv[1] + '&s=32';
+        img.classList.add('contributor-icon');
+        document.getElementById('contributors').appendChild(img);
+    });
+    document.getElementById('last-change').innerHTML += 'Last change: ' +
+    '<em>' + j[0].commit.message + '</em>' + ' by ' + j[0].commit.author.name + ' on ' + (new Date(j[0].commit.author.date)).toLocaleString();
 });
 
+makeHeadersCollapsible();
+generateSectionNavigation();
 feather.replace();

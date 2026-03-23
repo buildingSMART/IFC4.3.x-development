6a61bfefef8a2bc54f0c78140afa4f463a10cff6
Thomas Krijnen <t.krijnen@gmail.com>
2026-03-23 09:34:40 +0100

    Modularize XMI and encode Express specifics without tags and stereotypes

--- code/express.py
+++ code/express.py
@@ -1,31 +1,18 @@
 import logging
 
-express_basic_types = {"REAL", "NUMBER", "BINARY", "BOOLEAN", "INTEGER", "STRING", "LOGICAL"}
-
-def ifc_name(name):
-    if name.startswith("ENUMERATION"): return name
-    if name.startswith("SELECT"): return name
-    if name.startswith("Ifc"): return name
-    for ebt in express_basic_types: 
-        if ebt in name: return name
-    name = "".join([" ", c][c.isalnum()] for c in name)
-    s = "".join((s[0:1].upper() + s[1:].lower()) for s in name.split(" "))    
-    if not s.lower().startswith("ifc"):
-        s = "Ifc" + s
-    return s
-    
+_join = lambda x: map(lambda tup: "\t%s : %s;" % tup, x)
 
 class simple_type:
-    def __init__(self, name, super, constraints, super_verbatim):
-        self.name, self.super, self.constraints, self.super_verbatim = name, super, list(constraints), super_verbatim
+    def __init__(self, name, super, constraints):
+        self.name, self.super, self.constraints = name, super, list(constraints)
     
     def to_express(self):
         cs_tuple = []
         if len(self.constraints) != 0:
             cs_tuple.append(" WHERE")
-            cs_tuple.extend(self.constraints)
+            cs_tuple.extend(_join(self.constraints))
         return "\n".join((
-            "TYPE %s = %s;" % (ifc_name(self.name), self.super if self.super_verbatim else ifc_name(self.super)),
+            "TYPE %s = %s;" % (self.name, self.super),
         ) + tuple(cs_tuple) + (
             "END_TYPE;",
         ))    
@@ -41,7 +28,7 @@ class enum_or_select:
         
     def to_express(self):
         return "\n".join((
-            "TYPE %s = %s" % (ifc_name(self.name), self.type),
+            "TYPE %s = %s" % (self.name, self.type),
             "\n".join(map(lambda v: "\t%s%s" % (",("[v[0] == 0], v[1]), enumerate(self.values))) + ");",
             "END_TYPE;"
         ))
@@ -72,21 +59,21 @@ class entity:
     
             
     def to_express(self):
-        join = lambda x: map(lambda tup: "\t%s : %s;" % tup, x)
-        adic = map(list, (join(self.attributes), self.derived, self.inverses, join(self.unique_clauses), join(self.where_clauses)))
+        adic = map(list, map(_join, (self.attributes, self.derived, self.inverses, self.unique_clauses, self.where_clauses)))
         adic_labels = None, "DERIVE", "INVERSE", "UNIQUE", "WHERE"
         abstract_string = ["", " ABSTRACT"][self.is_abstract]
         
         def _():
             semi = "" if len(self.subtypes) or len(self.supertypes) else ";"
-            yield ("ENTITY %s" % ifc_name(self.name)) + semi
+            yield ("ENTITY %s" % self.name) + semi
             if len(self.subtypes) > 1:
                 logging.warning("Multiple super types for %s", self.name)
+                breakpoint()
             if len(self.supertypes):
                 semi = "" if len(self.subtypes) else ";"
-                yield "%s SUPERTYPE OF (ONEOF\n%s))" % (abstract_string, "\n".join(map(lambda v: "\t%s%s" % (",("[v[0] == 0], ifc_name(v[1])), enumerate(sorted(self.supertypes))))) + semi
+                yield "%s SUPERTYPE OF (ONEOF\n%s))" % (abstract_string, "\n".join(map(lambda v: "\t%s%s" % (",("[v[0] == 0], v[1]), enumerate(sorted(self.supertypes))))) + semi
             if len(self.subtypes) == 1:
-                yield " SUBTYPE OF (%s);" % ifc_name(self.subtypes[0])
+                yield " SUBTYPE OF (%s);" % self.subtypes[0]
             for label, li in zip(adic_labels, adic):
                 if label: 
                     if len(li):
